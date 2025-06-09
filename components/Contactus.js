8d1f9309f9730e8542a14e958cd111c91679a7f2
Sky28702 <prateekrai462@outlook.com>
2025-06-09 19:55:18 +0530

    fixcontact

--- components/Contactus.js
+++ components/Contactus.js
@@ -1,19 +1,26 @@
 "use client";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
 
 const Contact = () => {
-  const { register, handleSubmit, watch } = useForm();
-  const [data, setData] = useState("");
+  const { register, handleSubmit } = useForm();
+
+  function submit(data) {
+    const existingData =
+      JSON.parse(localStorage.getItem("contactFormData")) || [];
+    const updatedData = [...existingData, data];
+
+    localStorage.setItem("contactFormData", JSON.stringify(updatedData));
+    console.log("Form submitted and saved:", updatedData);
+  }
 
   return (
-    <section className="mb-[80px]  " id="Contact">
+    <section className="mb-[80px]" id="Contact">
       <h2 className="font-semibold text-[30px] text-blue-600 text-center mb-[20px]">
-        <u> Contact </u>
+        <u>Contact</u>
       </h2>
       <form
-        onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
-        className="flex flex-col justify-between items-center md:flex md:flex-col md:justify-between md:items-center  "
+        onSubmit={handleSubmit(submit)}
+        className="flex flex-col justify-between items-center"
       >
         <input
           {...register("name")}
@@ -23,7 +30,7 @@ const Contact = () => {
           placeholder="Name"
           required
           className="text-sm mb-8 border border-solid border-slate-200 rounded-[4px] py-2 px-2 w-60"
-        ></input>
+        />
 
         <input
           {...register("email")}
@@ -33,16 +40,16 @@ const Contact = () => {
           placeholder="Email"
           required
           className="text-sm mb-8 border border-solid border-slate-200 rounded-[4px] py-2 px-2 w-60"
-        ></input>
+        />
 
         <textarea
-          {...register("Contact Message: ")}
+          {...register("message")}
           className="text-sm h-36 w-60 mb-8 border border-solid border-slate-200 rounded-[4px] py-2 px-2"
           placeholder="Type your message..."
         ></textarea>
 
         <button
-          value="submit"
+          type="submit"
           className="cursor-pointer text-white bg-blue-600 py-1 px-5 rounded-[6px]"
         >
           Send
