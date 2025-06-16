a10af787dc1a0760c42963da6d4e224fcd64ce5a
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-06-16 23:19:13 +0530

    formfixes

---
+++ app/backend/actions/profile.js
@@ -0,0 +1,16 @@
+import Profile from "../models/profile";
+import dbConnect from "@/app/backend/db/dbConnect";
+
+async function createProfile(data) {
+  await dbConnect();
+  const profile = new Profile({
+    firstName: data.firstName,
+    lastName: data.lastName,
+    email: data.email,
+    address: data.address,
+  });
+  await profile.save();
+
+  console.log(profile);
+}
+export default createProfile;
---
+++ app/backend/models/profile.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const profileSchema = new mongoose.Schema({
+  image: { type: String, required: false },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, lowercase: true },
+  phoneNumber: { type: Number, min: 10, max: 12 },
+  address: { type: String, required: true },
+});
+
+const Profile = mongoose.model("Profile", profileSchema);
+export default Profile;
